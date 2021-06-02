47a65093da57b68f006b9654a587dfb8d3ec2ed2
Donovan <divdonis@gmail.com>
2021-06-01 20:03:42 -0700

    commit tree ee5879a2875d526f579e7f8cc7dd988fd25ceeeb

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+let data = {
+    name: "Steve",
+    email: "steve@steve.com"
+  };
+
+const config = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
+  body: JSON.stringify(data),
+} 
+function submitData(){ fetch("http://localhost:3000/users", config)
+
+  .then(response => response.json())
+  .then(data => {
+      console.log(data)
+  .catch(err => {
+      alert("Something's Up", err);
+  })
+}
+  ); 
+}
